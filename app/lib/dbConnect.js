259bf0bbea54a775b46476458b99900dfad0898e
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-01-17 21:42:12 +0700

    feat (env): connect to railway

--- app/lib/dbConnect.js
+++ app/lib/dbConnect.js
@@ -1,13 +1,16 @@
 "use strict";
 
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  port: 3307,
-  database: "bigio",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
 });
 
 export default dbPool;
