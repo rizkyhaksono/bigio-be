a0bb423a6ef138b1ceb68ab193bcd6df70ace43f
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-01-18 10:18:46 +0700

    feat (api): connect to sql railway

--- app/lib/dbConnect.js
+++ app/lib/dbConnect.js
@@ -7,18 +7,18 @@ dotenv.config();
 
 const dbPool = mysql.createPool({
   // cloud
-  // host: process.env.MYSQLHOST,
-  // user: process.env.MYSQLUSER,
-  // password: process.env.MYSQLPASSWORD,
-  // port: process.env.MYSQLPORT,
-  // database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
 
   // local
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_DATABASE,
 });
 
 export default dbPool;
